edf3fda8f1bd7c78c5598ed1d116f6285885f1a2
AveZorgen <78862872+AveZorgen@users.noreply.github.com>
2022-09-10 00:16:45 +0300

    del git artifacts

--- solidity/scripts/deploy.js
+++ solidity/scripts/deploy.js
@@ -7,27 +7,10 @@
 const hre = require("hardhat");
 
 async function main() {
-<<<<<<< HEAD
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Garant");
-  const lock = await Lock.deploy();
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-=======
     const Escrow = await hre.ethers.getContractFactory("Escrow");
     const escrow = await Escrow.deploy();
     await escrow.deployed();
     console.log(`deployed at ${escrow.address}`);
->>>>>>> 1043af900021a2cf93c95597b9725d0eff0bd02d
 }
 
 main().catch((error) => {
