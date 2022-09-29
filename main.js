6f16f739ec063a0a7a59097eb55695ed9f2e06f2
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2022-09-29 18:16:05 +0300

    clear code

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ const app = express();
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs'
-})
+});
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views', 'views')
+app.set('views', 'views');
 
 app.use(express.static(__dirname + '/public/'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -61,11 +61,6 @@ app.post("/fetch/updateDealStatus", async (req, res) => {
   answer = await dbUpdateDealStatus(req.body.id, req.body.status);
   res.send(answer);
 });
-
-// app.use((req, res) => {
-//   res.status(404);
-//   res.render('404 - Error');
-// });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
