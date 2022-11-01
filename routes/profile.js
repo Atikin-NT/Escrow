33d505887ab59e76753675f916cf7b00dd77d49a
AveZorgen <78862872+AveZorgen@users.noreply.github.com>
2022-11-01 08:34:34 +0300

    add cookie signin check

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,37 @@
 const { GoogleSignIn } = require('../lib/sqlite.js');
-const jwt_decode = require("jwt-decode");
+const { OAuth2Client } = require('google-auth-library');
 
 async function SignIn(req, res){
-    console.log(req.body.credential);
-    console.log(req.body.g_csrf_token);
-    //need to check req.body.g_csrf_token in cookies or smth
-    const decoded = jwt_decode(req.body.credential)
-    answer = await GoogleSignIn(decoded.sub,decoded.name,decoded.email,decoded.picture);
+    const CLIENT_ID = "748097504037-h7g5a4cqoj65keosu0mbfv4rumm0hf6i.apps.googleusercontent.com";
+    const token = req.body.credential;
+
+    const csrf_token_cookie = req.cookies.g_csrf_token;
+    if (csrf_token_cookie === undefined){
+        res.status(400).send('<h1>No CSRF token in Cookie.</h1>');
+        return;
+    }
+    const g_csrf_token = req.body.g_csrf_token;
+    if (g_csrf_token === undefined){
+        res.status(400).send('<h1>No CSRF token in post body.</h1>');
+        return;
+    }
+    if (csrf_token_cookie !== g_csrf_token){
+        res.status(400).send('<h1>Failed to verify double submit cookie.</h1>')
+        return;
+    }
+
+    const client = new OAuth2Client(CLIENT_ID);
+    async function verify() {
+      const ticket = await client.verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID,
+      });
+      const payload = ticket.getPayload();
+      console.log(payload)
+      return payload;
+    }
+    const payload = await verify().catch(console.error);
+    const answer = await GoogleSignIn(payload.sub, payload.name, payload.email, payload.picture);
     console.log(answer);
     res.redirect('profile');
 }
