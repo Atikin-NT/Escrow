4d95a317679deb4622ff4890a75a9369b859e049
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2022-11-05 21:44:05 +0300

    feat: title change

--- routes/fetchContentUpdate.js
+++ routes/fetchContentUpdate.js
@@ -32,6 +32,7 @@ async function changeDealView(req, res){
     unitListWithSelect[dbAnswer.unit] = "selected";
     res.render('partials/inputLayout', {
         layout : 'part',
+        title: 'Change Form',
         buyerCheck: buyerCheck,
         sellerCheck: sellerCheck,
         partner: partner, 
@@ -58,6 +59,7 @@ async function approveByPartnerView(req, res){
     if(dbAnswer.seller == account) role = "Seller";
     res.render('partials/textLayout', {
         layout : 'part',
+        title: 'Waiting for approved by your partner',
         role: role,
         seller: dbAnswer.seller, 
         buyer: dbAnswer.buyer, 
@@ -73,19 +75,18 @@ async function approveByPartnerView(req, res){
 }
 
 async function changeDealStatus(req, res){
-    console.log("Start changeDealStatus---------------");
     const id = parseInt(req.query.dealid);
     const account = req.query.account.toUpperCase();
     const newStatus = parseInt(req.query.status);
     let dbAnswer = defaulDeal;
+    let title = "default";
     if(id != undefined && id != null && id >= 0){
         const answer = JSON.parse(await dbGetDealsByID(id));
         console.log("answer.list[0].status = ", answer.list[0].status, " newStatus-1 = ", newStatus-1);
         if(answer.code == 0){
-            console.log(newStatus);
             switch(newStatus){
                 case 1:
-                    console.log(answer);
+                    title = "Have Been Aproved By Your Partner";
                     if(answer.list[0].status == newStatus-1 && (answer.list[0].sellerIsAdmin == 0 && answer.list[0].seller == account) || (answer.list[0].sellerIsAdmin == 1 && answer.list[0].buyer == account)){
                         const changeDealStatusAnswer = JSON.parse(await dbUpdateDealStatus(id, newStatus)).code;
                         if(changeDealStatusAnswer == 0){
@@ -96,7 +97,7 @@ async function changeDealStatus(req, res){
                     else dbAnswer = answer.list[0];
                     break;
                 case 2:
-                    console.log(answer);
+                    title = "Send Ethers by buyer";
                     if(answer.list[0].status == newStatus-1 && answer.list[0].buyer == account){
                         //IDEA: blockchain send money
                         const changeDealStatusAnswer = JSON.parse(await dbUpdateDealStatus(id, newStatus)).code;
@@ -108,7 +109,7 @@ async function changeDealStatus(req, res){
                     else dbAnswer = answer.list[0];
                 break;
                 case 3:
-                    console.log(answer);
+                    title = "Send magic box by seller";
                     if(answer.list[0].status == newStatus-1 && answer.list[0].seller == account){
                         //IDEA: blockchain send box
                         const changeDealStatusAnswer = JSON.parse(await dbUpdateDealStatus(id, newStatus)).code;
@@ -120,7 +121,7 @@ async function changeDealStatus(req, res){
                     else dbAnswer = answer.list[0];
                 break;
                 case 4:
-                    console.log(answer);
+                    title = ":)";
                     if(answer.list[0].status == newStatus-1 && answer.list[0].buyer == account){
                         //IDEA: blockchain approve that box is with buyer
                         const changeDealStatusAnswer = JSON.parse(await dbUpdateDealStatus(id, newStatus)).code;
@@ -132,6 +133,7 @@ async function changeDealStatus(req, res){
                     else dbAnswer = answer.list[0];
                 break;
                 default:
+                    title = "Something went wrong :(";
                     dbAnswer = answer.list[0];
                     break;
             }
@@ -141,6 +143,7 @@ async function changeDealStatus(req, res){
     if(dbAnswer.seller == account) role = "Seller";
     res.render('partials/textLayout', {
         layout : 'part',
+        title: title,
         role: role,
         seller: dbAnswer.seller, 
         buyer: dbAnswer.buyer, 
@@ -153,7 +156,6 @@ async function changeDealStatus(req, res){
         change: false,
         btnName: "Send Money Step",
     });
-    console.log("End changeDealStatus-----------------");
 }
 
 module.exports = { 
